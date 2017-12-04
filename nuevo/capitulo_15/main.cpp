162d7644a07d506fb3046e74f0138476daa4842c
iper <prog@classe.net>
2017-12-04 15:48:24 +0100

    rama_nueva_vieja

---
+++ nuevo/capitulo_15/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: prog
+ *
+ * Created on 4 de diciembre de 2017, 15:48
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
